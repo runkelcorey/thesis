756a63d994de7558805f7e2a06e342a4ba024054
Corey Runkel <runkelcorey@gmail.com>
2020-04-07 12:07:56 -0400

    Commited to models. Deleted extraneous TeX stylesheets. Uploaded .csv of data.

--- analysis.R
+++ analysis.R
@@ -7,19 +7,45 @@
 #libraries
 library(sf)
 library(tigris)
+library(betareg)
 library(spdep)
 library(spatialreg)
 set.seed(22903)
 
 
-#naïve regressions: residuals decidedly out-of-whack
-r <- lm(RPCT ~ FORQ3*DENSITY + PRICECHG + UNEM10 + VAP_B + VAP_NA + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + OLD + WHITESOMECOL + NSP, wide)
+#linear regressions: residuals decidedly out-of-whack
+##full-sample
+mod1 <- lm(RPCT ~ NSP1 + DISTNSP1 + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, wide)
+mod2 <- lm(RPCT ~ NSP1 + DISTNSP1 + PRICECHG0810*OWNEROCC + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, wide)
+
+##justp
+mod3 <- lm(RPCT ~ NSP1 + DISTNSP1 + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT != "EXTRA-TP"))
+mod4 <- lm(RPCT ~ NSP1 + DISTNSP1 + PRICECHG0810*OWNEROCC + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT != "EXTRA-TP"))
+
+  mod5 <- lm(RPCT ~ NSP1 + DISTNSP1 + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT == "WIN"))
+  mod6 <- lm(RPCT ~ NSP1 + DISTNSP1 + PRICECHG0810*OWNEROCC + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT == "WIN"))
+
+  mod7 <- lm(RPCT ~ NSP1 + DISTNSP1 + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT == "LOSE"))
+  mod8 <- lm(RPCT ~ NSP1 + DISTNSP1 + PRICECHG0810*OWNEROCC + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT == "LOSE"))
+
+#beta regressions
+wide <- mutate(wide, RPCT = ifelse(RPCT > 99.9, RPCT - .1, ifelse(RPCT < .1, RPCT + .1, RPCT))/100)
+
+mod9 <- betareg(RPCT ~ NSP1 + DISTNSP1 + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, wide)
+mod10 <- betareg(RPCT ~ NSP1 + DISTNSP1 + PRICECHG0810*OWNEROCC + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, wide)
+
+mod11 <- betareg(RPCT ~ NSP1 + DISTNSP1 + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT != "EXTRA-TP"))
+mod12 <- betareg(RPCT ~ NSP1 + DISTNSP1 + PRICECHG0810*OWNEROCC + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT != "EXTRA-TP"))
+
+  mod13 <- betareg(RPCT ~ NSP1 + DISTNSP1 + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT == "LOSE"))
+  mod14 <- betareg(RPCT ~ NSP1 + DISTNSP1 + PRICECHG0810*OWNEROCC + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT == "LOSE"))
+
+  mod15 <- betareg(RPCT ~ NSP1 + DISTNSP1 + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT == "WIN"))
+  mod16 <- betareg(RPCT ~ NSP1 + DISTNSP1 + PRICECHG0810*OWNEROCC + OWNEROCC*FORQ3 + UNEMCHG + MEDHHI + OLD + VAP_B + VAP_H + VAP_H2 + VAP_W*WHITESOMECOL + EVANRATE + LDSRATE + STATE, filter(wide, RESULT == "WIN"))
+
 
 #spatial dependence check
-spwide <- as_Spatial(wide, IDs = GEOID10) #return to sp object
-
-##regression
-s <- lm(RPCT ~ FORQ3*DENSITY + PRICECHG + UNEM10 + VAP_B + VAP_NA + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + OLD + WHITESOMECOL + NUMROUNDS, spwide@data)
+spwide <- as_Spatial(st_as_sf(wide, coords = c("LONGITUDE", "LATITUDE"), crs = 4269), IDs = GEOID10) #return to sp object
 
 ##create neighbors list
 nbwide <- tri2nb(spwide, row.names = spwide$GEOID10)
@@ -30,23 +56,4 @@ binary <- nb2listw(nbwide, style = "B", zero.policy = TRUE)
 
 ##spaital dependence tests on PCTR: all three reject conclusively the idea of no spatial dependence and indicate positive spatial correlation of RPCT
 lm.morantest(mod1, standardized, alternative = "two.sided", zero.policy = T)
-geary.test(spwide@data$RPCT, binary, zero.policy = T, alternative = "two.sided")
-
-#spatial regression models
-##lag
-slag <- lagsarlm(RPCT ~ NSP1*PRICECHG0810 + NSP2*PRICECHG0810 + SDQ*HICOST  + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W + STATE, spwide@data, standardized, type = "lag", zero.policy = FALSE)
-
-#models
-mod1 <- lm(RPCT ~ NSP1*PRICECHG0810 + NSP2*PRICECHG0810 + SDQ*HICOST + DISTNSP1 + DISTNSP2 + DISTNSP3 + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM05 + OLD + WHITESOMECOL*VAP_W + STATE, wide)
-mod2 <- lm(RPCT ~ NSP + FORQ3*DENSITY + PRICECHG + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W, spwide@data)
-
-mod3 <- lm(RPCT ~ NSP + FORQ3*DENSITY + PRICECHG + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W + STATE, filter(spwide@data, RESULT != "EXTRA-TP"))
-mod4 <- lm(RPCT ~ NSP + FORQ3*DENSITY + PRICECHG + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W, filter(spwide@data, RESULT != "EXTRA-TP"))
-mod5 <- lm(RPCT ~ NSP + FORQ3*DENSITY + PRICECHG + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W, filter(spwide@data, RESULT == "LOSE"))
-mod6 <- lm(RPCT ~ NSP + FORQ3*DENSITY + PRICECHG + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W, filter(spwide@data, RESULT == "WIN"))
-
-spwide@data <- mutate(spwide@data, RPCT = ifelse(RPCT < .05, RPCT + .05, ifelse(RPCT > .95, RPCT - .05, RPCT))/100)
-mod7 <- betareg(RPCT ~ NSP + FORQ3*DENSITY + PRICECHG + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W, spwide@data)
-mod8 <- betareg(RPCT ~ NSP + FORQ3*DENSITY + PRICECHG + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W, dplyr::filter(spwide@data, RESULT != "EXTRA-TP"))
-mod9 <- betareg(RPCT ~ NSP + FORQ3*DENSITY + PRICECHG + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W, filter(spwide@data, RESULT == "LOSE"))
-mod10 <- betareg(RPCT ~ NSP + FORQ3*DENSITY + PRICECHG + VAP_B + EVANRATE + LDSRATE + VAP_H + VAP_H2 + MEDHHI + OWNEROCC + UNEM10 + OLD + WHITESOMECOL*VAP_W, filter(spwide@data, RESULT == "WIN"))
+geary.test(mod1$residuals, binary, zero.policy = T, alternative = "two.sided")
